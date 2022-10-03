281944b16628478e88a294a4f56b2b8bd88eecab
Johanna Erikson <johannaERIkson@users.noreply.github.com>
2022-10-03 15:38:35 +0300

    esimese nelja tunni tööd

---
+++ kalasalat.py
@@ -0,0 +1,6 @@
+parool = "kalasalat"
+vastus = input("Sisesta parool ")
+while vastus != parool:
+    print("Parool on vale. Programmi ei saa edasi kasutada.")
+    vastus = input("Sisesta parool uuesti ")
+print("Lأµpuks أµige! Head kasutamist!!")
---
+++ read_comments.php
@@ -0,0 +1,49 @@
+<?php
+
+		require_once "../config.php";
+		
+		//loon andmebaasiga ühenduse
+		//server, kasutaja, parool, andmebaasiga
+		$conn = new mysqli ($server_host, $server_user_name, $server_password, $database);
+		//määran suhtlemisel kasutatava kooditabeli
+		$conn->set_charset("utf8");
+		
+		//valmistame ette andmete saatmise SQL käsu
+		$stmt = $conn->prepare("SELECT comment, grade, added from vp_daycomment");
+		echo $conn->error;
+		
+		//seome saadavad andmed muutujatega
+		$stmt ->bind_result($comment_from_db, $grade_from_db, $added_from_db);
+		//täidame käsu
+		$stmt->execute();
+		//kui saan ühe kirja
+		//if ($stmt->fetch()){
+			//mis selle ühe kirjega teha
+		//}
+		//kui tuleb teadmata arv kirjeid
+		$comment_html =null;
+		while ($stmt->fetch()){
+			//echo $comment_from_db; 
+			//<p>kommentaar, hinne päevale: 6, lisatud xxxxxxxxx</p>
+			$comment_html .= "<p>" .$comment_from_db .", hinne päevale: ".$grade_from_db;
+			$comment_html.= ", lisatud ". $added_from_db .".</p> \n";
+			
+		}
+		
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Johanna Erikson programmeerib veebi</title>
+</head>
+<body>
+<h1>Johanna Erikson programmeerib veebi</h1>
+<p>See leht on loodud õppetöö raames ja ei sisalda tõsiselt võetavat sisu!</p>
+<p> Õppetöö toimus <a href="https://www.tlu.ee" target="_blank">Tallinna ülikoolis</a> Digitehnoloogiate instituudis.</p>
+<?php echo $comment_html; ?>
+
+
+</body>
+</html>
