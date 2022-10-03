d994ce575aeb19c49cb865fe23d55eb8469029ac
Johanna Erikson <johannaERIkson@users.noreply.github.com>
2022-10-03 15:49:07 +0300

    esimese nelja tunni tööd

--- kalasalat.py
+++
@@ -1,6 +0,0 @@
-parool = "kalasalat"
-vastus = input("Sisesta parool ")
-while vastus != parool:
-    print("Parool on vale. Programmi ei saa edasi kasutada.")
-    vastus = input("Sisesta parool uuesti ")
-print("Lأµpuks أµige! Head kasutamist!!")
